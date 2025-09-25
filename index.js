0c6d12eefd96151b915775b5ebef5a84160927b5
tugascrown204 <gajdertevet@hotmail.com>
2025-09-25 18:05:33 +0330

    Update version to 1.1.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const { program } = require('commander');
 
 program
-  .version('1.0.0')
+  .version('1.1.0')
   .command('analyze <path>')
   .description('Run code analysis')
   .action((path) => {
